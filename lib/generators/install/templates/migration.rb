160268e75edb40f2b3f71aa34b673d3055041d28
Nobuhiro Nikushi <deneb.ge@gmail.com>
2017-11-09 15:18:34 +0900

    Support migration version introduced from Rails 5.0

--- lib/generators/install/templates/migration.rb
+++ lib/generators/install/templates/migration.rb
@@ -1,4 +1,4 @@
-class CreateTaskRecords < ActiveRecord::Migration
+class CreateTaskRecords < ActiveRecord::Migration<%= ActiveRecord::VERSION::MAJOR >= 5 ? "[#{ActiveRecord::Migration.current_version}]" : '' %>
   def change
     create_table :<%= table_name %>, :id => false do |t|
       t.string :version, :null => false
